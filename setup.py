6cde1b786ad4b500f181306b40373726a446a091
keithcampbelljr <keithcampbelljr@icloud.com>
2024-09-09 14:25:56 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -5,13 +5,19 @@ setup(
     version='0.1',
     packages=find_packages(include=['app', 'app.*']),
     install_requires=[
-        'azure-identity',
         'azure-mgmt-authorization',
+        'azure-mgmt-msi',
+        'azure-identity',
+        'azure-core',
         'click',
+        'typing-extensions'
     ],
     entry_points={
         'console_scripts': [
             'mi-inspect=app.cli:main',
         ],
     },
+    description='A tool for checking azure roles associated with a managed identity',
+    author='Keith Campbell',
+    author_email='keithcampbelljr@icloud.com',
 )
